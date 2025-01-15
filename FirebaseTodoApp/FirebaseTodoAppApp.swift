d3299a2c20dca42ac75002ab9e317f489039d35b
Alena <alenabelovadmvn@gmail.com>
2025-01-15 15:21:53 -0500

    Initial Commit

---
+++ FirebaseTodoApp/FirebaseTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseTodoAppApp.swift
+//  FirebaseTodoApp
+//
+//  Created by Alena Belova  on 2025-01-15.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
